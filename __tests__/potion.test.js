cd3da4dda6070c43a7a0d46ae7704f0044a3a9e1
Varsha Jain <csvarshajain@gmail.com>
2021-10-20 16:47:12 -0400

    added player object and method

--- __tests__/potion.test.js
+++ __tests__/potion.test.js
@@ -1,6 +1,13 @@
 const Potion = require('../lib/Potion.js');
 
-test('Create a health potion object', () => {
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random potion object', () => {
     const potion = new Potion();
 
     expect(potion.name).toEqual(expect.any(String));
